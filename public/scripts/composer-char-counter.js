c400e9e33d59d2c1351815c16b56f4c9a008bfbe
TJ <teejaynam@gmail.com>
2023-07-09 16:38:50 -0400

    add jquery eventlistener for tweet character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+    const maxCount = 140;
+    const currentCount = $(this).val().length;
+    const counter = $(this).closest('.new-tweet').find('.counter');
+    let countRemainder = maxCount - currentCount;
+
+    counter.text(countRemainder);
+
+    if ( countRemainder < 0 ) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', '');
+    }
+  });
+});
