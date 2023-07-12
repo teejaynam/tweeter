5ce5894d7dfc13c1ac7f2c7a0442f3e7113494fb
TJ <teejaynam@gmail.com>
2023-07-12 19:39:15 -0400

    add function to render posted tweets and to append tweets to article

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,61 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
+
+const renderTweets = function(tweets) {
+  tweets.forEach((tweet) => {
+    const oldTweet = createTweetElement(tweet);
+    console.log(oldTweet);
+    $('.posted-tweets').append(oldTweet);
+  })
+}
+
+const createTweetElement = function(tweetData){
+  let $tweet = $(`<article class="tweet">`);
+
+  $tweet.append(`
+    <header>
+      <h3 class="name">${tweetData.user.name}</h3>
+      <h4 class="handle">${tweetData.user.handle}</h4>
+    </header>
+    <body>
+      <p>${tweetData.content.text}</p>
+    </body>
+    <footer>
+      <div class="timestamp">${tweetData.created_at}</div>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </footer>
+  `);
+
+
+  return $tweet;
+}
+
+$(document).ready(function() {
+  renderTweets(data);
+});
